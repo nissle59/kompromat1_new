9f999c5a5144d965e41ba6a45bcfabeed2f4b0f4
nissle59 <nixncom@gmail.com>
2023-03-27 09:34:59 +0300

    Upd

--- sql_migration.py
+++ sql_migration.py
@@ -158,22 +158,18 @@ def clear_article(url, html) -> dict:
     if_count = 1
     iframes = []
     for iframe in article.find_all('iframe'):
-        if iframe['src'][:2] == '//':
-            iframe_src = 'https:' + iframe['src']
-        else:
-            iframe_src = iframe['src']
-        a = BeautifulSoup(f'<a target="_blank">| Источник №{if_count} |</a>',
-                          features="html.parser")
-        a.a['href'] = iframe_src.replace('\"', '').replace('\\', '')
-        # iframe.attrs = {}
-        # iframe.name = 'a'
-        # iframe['href'] = iframe_src
-        # iframe['target'] = '_blank'
-
-        # iframe.replace_with(a)
+        if 'src' in iframe.keys():
+            if iframe['src'][:2] == '//':
+                iframe_src = 'https:' + iframe['src']
+            else:
+                iframe_src = iframe['src']
+            a = BeautifulSoup(f'<a target="_blank">| Источник №{if_count} |</a>',
+                              features="html.parser")
+            a.a['href'] = iframe_src.replace('\"', '').replace('\\', '')
+            iframes.append(a.a)
+            if_count += 1
         iframe.extract()
-        if_count += 1
-        iframes.append(a.a)
+
 
     v = soup.find_all('div')
     for div in v:
